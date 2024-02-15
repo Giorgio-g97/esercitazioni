f4e470454a020895e3bc57b03ad9ff00cee9923a
Giorgio G <giorgio.g97@gmail.com>
2024-02-15 17:46:37 +0100

    agg less innerHTML

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,13 @@
-const arrayList = [];
+const arrayList = ['wake up'];
+
+// Ora 9h
+
+for(let i= 0; i < arrayList.length; i++){
+  const task = arrayList[i];
+  const html = `<p>${task}</p>`
+  const resTask = document.querySelector('.js-todolist').innerHTML
+  resTask += html
+}
 
 const addList = () => {
   const inputName = document.querySelector(".input-js");
