606cff23de43efdb06c27fd752026a307e75c1dc
Giorgio G <giorgio.g97@gmail.com>
2024-02-21 17:39:45 +0100

    Agg grid

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,22 +1,46 @@
-const arrayList = ["watch youtube", "dinner"];
+const arrayList = [
+  {
+    name: "wash dishes",
+    date: "2024-02-21",
+  },
+];
 
 const updateList = () => {
   let pHTML = "";
   for (let i = 0; i < arrayList.length; i++) {
-    const task = arrayList[i]; // scorriamo array
-    const html = `<p>${task} <button>Delete</button></p>`; // creiamo html
+    const taskObj = arrayList[i];
+    // const name = taskObj.name;
+    // const date = taskObj.date;
+    const { name, date } = taskObj; // structuring, estrapola name dall'oggetto e la salva in una variabile con lo stesso nome
+    const html = `
+    <div>${name}</div>
+    <div>${date}</div>
+    ${name} ${date} 
+    <button onclick="
+      arrayList.splice(${i}, 1);
+      updateList();
+    "
+    >Delete</button>
+    `; // creiamo html
     pHTML += html; // salviamo in var.
-    document.querySelector(".js-todolist").innerHTML = pHTML;// modifichiamo il div inserendo l'html
-  }
+    document.querySelector(".js-todolist").innerHTML = pHTML; // modifichiamo il div inserendo l'html
+  };
 };
 
 updateList();
 
 const addList = () => {
   const inputName = document.querySelector(".input-js");
-  // console.log(inputName);
-  arrayList.push(inputName.value);
-  console.log(arrayList);
+  const inputDate = document.querySelector(".js-date-input");
+  const name = inputName.value;
+  const date = inputDate.value;
+  arrayList.push({
+    // name: inputName.value,
+    // date: inputDate.value
+    name, // shorthand, valore e prop hanno stesso nome
+    date,
+  });
   inputName.value = "";
+  inputDate.value = "";
   updateList();
 };
