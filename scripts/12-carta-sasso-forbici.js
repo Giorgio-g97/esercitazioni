b70b06823784f9fe8f435ec39fd37d63f4788d12
Giorgio G <giorgio.g97@gmail.com>
2024-02-23 17:31:57 +0100

    Autoplay rock-paper-scissors

--- scripts/12-carta-sasso-forbici.js
+++ scripts/12-carta-sasso-forbici.js
@@ -1,104 +1,111 @@
 let score = JSON.parse(localStorage.getItem("score")) || {
-    // Default operator
-    wins: 0,
-    losses: 0,
-    ties: 0,
-  };
+  // Default operator
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
 
-   updateScore();
+const autoPlay = () => {
+  setInterval(() => {
+    const playerRand = PCMove();
+    playGame(playerRand);
+  }, 1000);
+};
 
-  const playGame = (playerMove) => {
-    let computerNum = PCMove();
-    let result = "";
+updateScore();
 
-    if (playerMove === "rock") {
-      if (computerNum === "Rock") {
-        result = "Hai pareggiato";
-        computerNum = '✊';
-        playerMove = '✊';
-      } else if (computerNum === "Paper") {
-        result = "Hai perso";
-        computerNum = '✋';
-        playerMove = '✊';
-      } else if (computerNum === "Scissors") {
-        result = "Hai vinto";
-        computerNum = '✌️';
-        playerMove = '✊';
-      }
-    } else if (playerMove === "paper") {
-      if (computerNum === "Rock") {
-        result = "Hai vinto";
-        computerNum = '✊';
-        playerMove = '✋';
-      } else if (computerNum === "Paper") {
-        result = "Hai pareggiato";
-        computerNum = '✋';
-        playerMove = '✋';
-      } else if (computerNum === "Scissors") {
-        result = "Hai perso";
-        computerNum = '✌️';
-        playerMove = '✋';
-      }
-    } else if (playerMove === "scissors") {
-      if (computerNum === "Rock") {
-        result = "Hai perso";
-        computerNum = '✊';
-        playerMove = '✌️';
-      } else if (computerNum === "Paper") {
-        result = "Hai vinto";
-        computerNum = '✋';
-        playerMove = '✌️';
-      } else if (computerNum === "Scissors") {
-        result = "Hai pareggiato";
-        computerNum = '✌️';
-        playerMove = '✌️';
-      }
+const playGame = (playerMove) => {
+  let computerNum = PCMove();
+  let result = "";
+
+  if (playerMove === "Rock") {
+    if (computerNum === "Rock") {
+      result = "Hai pareggiato";
+      computerNum = "✊";
+      playerMove = "✊";
+    } else if (computerNum === "Paper") {
+      result = "Hai perso";
+      computerNum = "✋";
+      playerMove = "✊";
+    } else if (computerNum === "Scissors") {
+      result = "Hai vinto";
+      computerNum = "✌️";
+      playerMove = "✊";
     }
-
-    // Gestione punteggi
-
-    if (result === "Hai vinto") {
-      score.wins += 1;
+  } else if (playerMove === "Paper") {
+    if (computerNum === "Rock") {
+      result = "Hai vinto";
+      computerNum = "✊";
+      playerMove = "✋";
+    } else if (computerNum === "Paper") {
+      result = "Hai pareggiato";
+      computerNum = "✋";
+      playerMove = "✋";
+    } else if (computerNum === "Scissors") {
+      result = "Hai perso";
+      computerNum = "✌️";
+      playerMove = "✋";
     }
-    if (result === "Hai perso") {
-      score.losses += 1;
+  } else if (playerMove === "Scissors") {
+    if (computerNum === "Rock") {
+      result = "Hai perso";
+      computerNum = "✊";
+      playerMove = "✌️";
+    } else if (computerNum === "Paper") {
+      result = "Hai vinto";
+      computerNum = "✋";
+      playerMove = "✌️";
+    } else if (computerNum === "Scissors") {
+      result = "Hai pareggiato";
+      computerNum = "✌️";
+      playerMove = "✌️";
     }
-    if (result === "Hai pareggiato") {
-      score.ties += 1;
-    }
-
-    localStorage.setItem("score", JSON.stringify(score));
-
-    // Aggiorna il punteggio a display dopo averlo inserito
-    updateScore();
-
-    document.querySelector(".js-result").innerHTML = result;
-
-    document.querySelector(
-      ".js-move"
-    ).innerHTML = `Player Move ${playerMove} - ${computerNum} Computer Move`;
-  };
-
-  function updateScore() {
-    document.querySelector(
-      ".js-score"
-    ).innerHTML = `Vittorie: ${score.wins}, Perse: ${score.losses}, Pareggi: ${score.ties}`;
   }
 
+  // Gestione punteggi
+
+  if (result === "Hai vinto") {
+    score.wins += 1;
+  }
+  if (result === "Hai perso") {
+    score.losses += 1;
+  }
+  if (result === "Hai pareggiato") {
+    score.ties += 1;
+  }
+
+  localStorage.setItem("score", JSON.stringify(score));
+
+  // Aggiorna il punteggio a display dopo averlo inserito
+  updateScore();
+
+  document.querySelector(".js-result").innerHTML = result;
+
+  document.querySelector(
+    ".js-move"
+  ).innerHTML = `Player Move ${playerMove} - ${computerNum} Computer Move`;
+};
+
+function updateScore() {
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = `Vittorie: ${score.wins}, Perse: ${score.losses}, Pareggi: ${score.ties}`;
+}
+
 // PC genera numero randomico
-  const PCMove = () => {
-    // Variabile salvata all'interno dello scope funzione
-    let computerNum = "";
+const PCMove = () => {
+  // Variabile salvata all'interno dello scope funzione
+  let computerNum = "";
 
-    const randomNumber = Math.random();
+  const randomNumber = Math.random();
 
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-      computerNum = "Rock";
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-      computerNum = "Paper";
-    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-      computerNum = "Scissors";
-    }
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerNum = "Rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerNum = "Paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerNum = "Scissors";
+  }
 
-    return computerNum;
-  };
+  return computerNum;
+};
