1690b7cdf579420219b8c0173d40e513000c8e3b
Giorgio G <giorgio.g97@gmail.com>
2024-02-26 15:50:54 +0100

    rock-scissors-papaer switched to addEvList

--- scripts/12-carta-sasso-forbici.js
+++ scripts/12-carta-sasso-forbici.js
@@ -11,19 +11,67 @@ let intervalId;
 
 const autoPlay = () => {
   if (!isAutoPlaying) {
-    intervalId = setInterval(() => { // il setInterval genera un ID
+    intervalId = setInterval(() => {
+      // il setInterval genera un ID
       const playerRand = PCMove(); // 10:45h
       playGame(playerRand);
     }, 1000);
-    console.log(intervalId)
-    isAutoPlaying = true
-  }else{
-    clearInterval(intervalId) // stop interval prendendo l'ultimo ID
+    console.log(intervalId);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId); // stop interval prendendo l'ultimo ID
     isAutoPlaying = false;
   }
 };
 
 updateScore();
+
+// Sostituz. con addEventListener
+
+document.querySelector(".rock-btn").addEventListener("click", () => {
+  playGame("Rock");
+});
+
+document.querySelector(".paper-btn").addEventListener("click", () => {
+  playGame("Paper");
+});
+
+document.querySelector(".scissors-btn").addEventListener("click", () => {
+  playGame("Scissors");
+});
+
+document.querySelector(".score-btn").addEventListener("click", () => {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem("score");
+  updateScore();
+  res = document.querySelector(".js-result");
+  move = document.querySelector(".js-move");
+  res.innerHTML = '';// resetta i risultati
+  move.innerHTML = '';// resetta le giocate fatte
+});
+
+document.querySelector(".autoplay-btn").addEventListener("click", () => {
+  autoPlay();
+});
+
+//
+
+// Utilizzo keydown
+
+document.body.addEventListener('keydown', (e) => {// passiamo come parametro l'evento generato dalla pressione del tasto
+  console.log(e.key);// loggerà la chiave premuta
+  if(e.key === 'r'){
+    playGame("Rock");
+  }else if(e.key === 'p'){
+    playGame("Paper");
+  }else if(e.key === 's'){
+    playGame("Scissors");
+  }
+})
+
+//
 
 const playGame = (playerMove) => {
   let computerNum = PCMove();
