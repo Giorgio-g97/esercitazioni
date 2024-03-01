346f20bb19d43121082ddb2982b43ace5ee3f3e1
Giorgio G <giorgio.g97@gmail.com>
2024-03-01 17:15:44 +0100

    Finito Tris, da perfezionare

--- Projects (Optional)/project-2/prj-2.js
+++ Projects (Optional)/project-2/prj-2.js
@@ -1,8 +1,20 @@
 const resBtn = document.querySelector(".restart-btn");
 
-const playerMove = document.querySelector(".playerMove");
+const statusPlayer = document.querySelector(".statusPlayer");
 
-const winnerOptions = [
+const cells = document.querySelectorAll(".cell");
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+let move = "X";
+
+let isGameActive = true;
+
+const playerXWon = "Player X Won";
+const playerOWon = "Player O Won";
+const tie = "Tie";
+
+const winnerComb = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -13,39 +25,68 @@ const winnerOptions = [
   [2, 4, 6],
 ];
 
-let move = "Nessuno";
+let pMove;
 
-playerMove.innerHTML = `E' il turno di: ${move}`;
+statusPlayer.innerHTML = `E' il turno di: ${move}`;
 
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell, index) => {
-  console.log(index);
-  cell.addEventListener("click", () => {
-    playGame(cell);
+const startGame = () => {
+  cells.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+      playGame(cell, index);
+    });
   });
-});
-
-const playGame = (cell) => {
-  if (move === "Nessuno" || move === "X") {
-    cell.textContent = "X";
-    move = "O";
-    playerMove.textContent = `E' il turno di: ${move}`;
-  } else if (move === "O") {
-    cell.textContent = "O";
-    move = "X";
-    playerMove.textContent = `E' il turno di: ${move}`;
-  }
-  checkWin();
 };
 
+const playGame = (cell, index) => {
+  if (move === "X") {
+    cell.textContent = "X";
+    gameState[index] = move;
+    checkWin();
+    // gameOver();
+    move = "O";
+    statusPlayer.textContent = `E' il turno di: ${move}`;
+  } else if (move === "O") {
+    gameState[index] = move;
+    cell.textContent = "O";
+    checkWin();
+    move = "X";
+    statusPlayer.textContent = `E' il turno di: ${move}`;
+  }
+};
+
+function checkWin() {
+  let roundWin = false;
+  for (i = 0; i < winnerComb.length; i++) {
+    const win = winnerComb[i];
+    let a = gameState[win[0]];
+    let b = gameState[win[1]];
+    let c = gameState[win[2]];
+    if (a === "" || b === "" || c === "") {
+      continue;
+    }
+    if (a === b && b === c) {
+      roundWin = true;
+      console.log(`${move} ha vinto`);
+      restartGame();
+      break;
+    }
+  }
+}
+
+// function gameOver() {
+//   if(roundWin === true)
+// }
+
 const restartGame = () => {
-  move = "Nessuno";
-  cells.forEach((cell, index) => {
+  move = "X";
+  cells.forEach((cell) => {
     cell.textContent = "";
+    gameState = ["", "", "", "", "", "", "", "", ""];
   });
 
-  playerMove.textContent = `E' il turno di: ${move}`;
+  statusPlayer.textContent = `E' il turno di: ${move}`;
 };
 
 resBtn.addEventListener("click", restartGame);
+
+startGame();
