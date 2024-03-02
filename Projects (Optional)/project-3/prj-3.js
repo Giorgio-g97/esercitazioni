8400b2648d40ba927204f706c5d2b84fd1659af9
Giorgio <giorgio.g97@gmail.com>
2024-03-02 10:29:45 +0100

    Insert prj-3

---
+++ Projects (Optional)/project-3/prj-3.js
@@ -0,0 +1,52 @@
+const arrayList = JSON.parse(localStorage.getItem('arrayList')) || [];
+
+const updateList = () => {
+  let pHTML = "";
+
+  arrayList.forEach(function(taskObj, index) {
+    const { name, date } = taskObj; // destructuring, estrapola name dall'oggetto e la salva in una variabile con lo stesso nome
+    const html = `
+      <div>${name}</div>
+      <div>${date}</div>
+      <button class="del-btn">Delete</button>
+      `; // creiamo html
+      pHTML += html; // salviamo in var.
+  })
+    document.querySelector(".js-todolist").innerHTML = pHTML; // modifichiamo il div inserendo l'html
+
+    document.querySelectorAll('.del-btn')// prendi tutti i del-btn generati
+      .forEach((delBtn, index) => {// scorri con forEach
+        delBtn.addEventListener('click', () => {// aggiungi un eventListener
+          arrayList.splice(index, 1);// esegui il codice
+          updateList();
+        })
+      })
+};
+
+updateList();
+
+document.querySelector('.add-btn')
+  .addEventListener('click', () => {
+    addList();
+  })
+
+const addList = () => {
+  const inputName = document.querySelector(".input-js");
+  const inputDate = document.querySelector(".js-date-input");
+  const name = inputName.value;
+  const date = inputDate.value;
+  arrayList.push({ // pusha i valori nell'array
+    // name: inputName.value,
+    // date: inputDate.value
+    name, // shorthand, valore e prop hanno stesso nome
+    date,
+  });
+  inputName.value = "";
+  inputDate.value = "";
+  updateList();
+  saveLocalStorage();
+};
+
+const saveLocalStorage = () => {
+  localStorage.setItem('arrayList', JSON.stringify(arrayList))
+}
