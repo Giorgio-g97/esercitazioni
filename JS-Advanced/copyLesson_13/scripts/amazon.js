8775c1c6d4f2f36bc1bb17a312b8a2e85545967b
Giorgio G <giorgio.g97@gmail.com>
2024-03-04 15:09:02 +0100

    Add select function

--- JS-Advanced/copyLesson_13/scripts/amazon.js
+++ JS-Advanced/copyLesson_13/scripts/amazon.js
@@ -1,9 +1,8 @@
+let productsHTML = "";
 
-
-let productsHTML = '';
-
-products.forEach((product) => {// Accumulate method
-    productsHTML += `
+products.forEach((product) => {
+  // Accumulate method
+  productsHTML += `
         <div class="product-container">
         <div class="product-image-container">
         <img class="product-image"
@@ -27,7 +26,7 @@ products.forEach((product) => {// Accumulate method
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -54,37 +53,45 @@ products.forEach((product) => {// Accumulate method
         </button>
     </div>
     `;
-})
+});
 
-document.querySelector('.js-products-grid')
-    .innerHTML = productsHTML
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-    document.querySelectorAll('.js-add-to-cart')
-        .forEach((button) => {
-            button.addEventListener('click', () => {
-                const productId = button.dataset.productId;
-                let matchingItem;// var per salvare item già presente
-                cart.forEach(item => {// itera gli items del carrello
-                    if(productId === item.productId){// se il nome del prodotto che si vuole aggiungere è uguale al nome del prodotto presente già nel carrello
-                        matchingItem = item;//Inserisci nella var
-                    }
-                });
-                if(matchingItem){//se il match esiste
-                    matchingItem.quantity += 1 // incrementa solo la quantità
-                }else{
-                    cart.push({//altrimenti pusha nuovo item diverso
-                        productId: productId,
-                        quantity: 1
-                    })
-                }
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    let matchingItem; // var per salvare item già presente
+    cart.forEach((item) => {
+      // itera gli items del carrello
+      if (productId === item.productId) {
+        // se il nome del prodotto che si vuole aggiungere è uguale al nome del prodotto presente già nel carrello
+        matchingItem = item; //Inserisci nella var
+      }
+    });
 
-                let cartQuantity = 0;
+    const sel = document.querySelector(`.js-quantity-selector-${productId}`)
+    let selVal = Number(sel.value)
+    console.log(selVal);
 
-                cart.forEach(item => {
-                    cartQuantity += item.quantity
-                })
+    if (matchingItem) {
+      //se il match esiste
+      matchingItem.quantity += selVal; // incrementa solo la quantità
+    } else {
+      cart.push({
+        //altrimenti pusha nuovo item diverso
+        productId: productId,
+        quantity: selVal,
+      });
+    }
 
-                document.querySelector('.js-cart-quantity')
-                    .innerHTML = cartQuantity
-            })
-        })
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  });
+});
+
+document.querySelector
