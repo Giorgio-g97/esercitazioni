0be09e22e43403173c393d2d605ac8194d51d226
Giorgio G <giorgio.g97@gmail.com>
2024-03-14 08:14:19 +0100

    Riorganizzazione fiels

--- JS-Advanced/Lesson 13/tests-jasmine/utils/moneyTest.js
+++ JS-Advanced/Lesson 13/tests-jasmine/utils/moneyTest.js
@@ -1,16 +1,17 @@
-import { formatCurrency } from "../../scripts/utils/money.js"
+import { formatCurrency } from "../../scripts/utils/money.js";
 
-describe('Test suite: formatCurrency', () => {//describe() to create suite test
-    it('(Title test) Converts cents into dollars', () => {//to create test funct
-        expect(formatCurrency(2095)).toEqual('20.95');//compara valori (if) e stampa console.log() in un'unica riga
-    })
+describe("Test suite: formatCurrency", () => {
+  //describe() to create suite test
+  it("(Title test) Converts cents into dollars", () => {
+    //to create test funct
+    expect(formatCurrency(2095)).toEqual("20.95"); //compara valori (if) e stampa console.log() in un'unica riga
+  });
 
-    it('Works with zero (0)', () => {
-        expect(formatCurrency(0)).toEqual('0.00');
-    })
+  it("Works with zero (0)", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
 
-    it('Rounds up to the nearest cent', () => {
-        expect(formatCurrency(2000.5)).toEqual('20.01');
-    })
+  it("Rounds up to the nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
 });
-
