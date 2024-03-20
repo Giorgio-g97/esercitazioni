d5baa1a6b2bdc4a2df4cd293a495b0907d171987
Giorgio G <giorgio.g97@gmail.com>
2024-03-20 17:40:50 +0100

    Esercizio 02 finito

--- javascript-esercitazioni/esercizi-midali/02-arrays/script.js
+++ javascript-esercitazioni/esercizi-midali/02-arrays/script.js
@@ -1,2 +1,23 @@
-const myArr = [1,5,8,3,9,1,0];
+const myArr = [];
 
+const ul = document.createElement('ul')
+
+function getRandNum() {
+  let randNum;
+  for (i = 0; i < 10; i++) {
+    randNum = Math.floor(Math.random() * 100);
+    myArr.push(randNum);
+  }
+  console.log(myArr);
+}
+
+function verPari() {
+  let arrPari = [];
+  getRandNum();
+  for (let n in myArr) {
+    myArr[n] % 2 === 0 ?  arrPari.push(myArr[n]) : null
+  }
+  console.log(arrPari);
+}
+
+verPari();
