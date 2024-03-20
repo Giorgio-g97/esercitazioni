0a649929a627f4fdbd90eb086ca0f07171f1cc56
Giorgio G <giorgio.g97@gmail.com>
2024-03-20 08:38:28 +0100

    Primo esercizio fatto Midali

--- javascript-esercitazioni/esercizi-midali/01-date/script.js
+++ javascript-esercitazioni/esercizi-midali/01-date/script.js
@@ -4,8 +4,27 @@ const oggi = new Date();
 
 const options = [
   {
-      
+    weekday: "long",
   },
+  {
+    dateStyle: "short",
+  },
+  {
+    weekday: 'short'
+  }
 ];
+const firstDate =
+  oggi.toLocaleString("it-IT", options[0]) +
+  " " +
+  oggi.toLocaleString("it-IT", options[1]);
+dateArr.push(firstDate);
 
-console.log(oggi.toLocaleString("it-IT", options));
+const secondDate = oggi.toLocaleTimeString() + ' ' + oggi.toLocaleTimeString('it-IT', {hour12: true})
+dateArr.push(secondDate);
+
+const thirdDate = oggi.toLocaleString('it-IT', options[2]) + ' ' + oggi.toLocaleString("it-IT", options[1]);
+dateArr.push(thirdDate)
+
+// console.log(firstDate, secondDate, thirdDate);
+
+console.log(dateArr);
