ef58ef6fb8c1d6c80d9dc77e2f59dfaf3c6d7207
Giorgio G <giorgio.g97@gmail.com>
2024-03-21 12:02:09 +0100

    Esercizio 04 finito

--- javascript-esercitazioni/esercizi-midali/04-diff-date/scirpt.js
+++ javascript-esercitazioni/esercizi-midali/04-diff-date/scirpt.js
@@ -1,14 +1,17 @@
 const date2 = document.querySelector(".date2");
 const date1 = document.querySelector(".date1");
 function getDateValue(date) {
-  const dateObj = new Date(date.value);
-  const dateFormat = dateObj.toLocaleDateString('it-IT', {dateStyle: "short"});
-  console.log(dateFormat);
+  dateValue = date.value
+  const dateObjInMS = new Date(dateValue).getTime();//in ms
+  console.log(typeof dateObjInMS);
+  return dateObjInMS;
 }
 
 document.querySelector('.getValue').addEventListener('click', ()=>{
-    getDateValue(date2)
-    getDateValue(date1)
+    let date2Ms = getDateValue(date2)
+    let date1Ms = getDateValue(date1)
+    let dif = date2Ms-date1Ms
 
-    
+    let difference = Math.round(dif / (1000 * 60 * 60 * 24))
+    console.log(difference);
 })
