407cf62c537df8e7d2a3c2a62229232834c11803
Giorgio G <giorgio.g97@gmail.com>
2024-03-21 13:18:26 +0100

    Add null date control

--- javascript-esercitazioni/esercizi-midali/04-diff-date/scirpt.js
+++ javascript-esercitazioni/esercizi-midali/04-diff-date/scirpt.js
@@ -1,19 +1,25 @@
 const date2 = document.querySelector(".date2");
 const date1 = document.querySelector(".date1");
-const res = document.querySelector('.result');
+const res = document.querySelector(".result");
 function getDateValue(date) {
-  dateValue = date.value//Prendo il valore
-  const dateObjInMS = new Date(dateValue).getTime();//Ottengo l'oggetto data e lo converto in ms
-  console.log(typeof dateObjInMS);//Per verificare che non ci siano errori, consolloggo il tipo di dato
-  return dateObjInMS;//lo ritorno in modo da utilizzarlo per la funzione successiva
+  dateValue = date.value; //Prendo il valore
+  const dateObjInMS = new Date(dateValue).getTime(); //Ottengo l'oggetto data e lo converto in ms
+  console.log(typeof dateObjInMS); //Per verificare che non ci siano errori, consolloggo il tipo di dato
+  return dateObjInMS; //lo ritorno in modo da utilizzarlo per la funzione successiva
 }
 
-document.querySelector('.getValue').addEventListener('click', ()=>{
-    let date2Ms = getDateValue(date2)//IMPORTANTE, salvo il risultato della funzione (che è un numero) in una variabile
-    let date1Ms = getDateValue(date1)
-    let dif = date2Ms-date1Ms//Faccio differenza
+document.querySelector(".getValue").addEventListener("click", () => {
+  let date2Ms = getDateValue(date2); //IMPORTANTE, salvo il risultato della funzione (che è un numero) in una variabile
+  let date1Ms = getDateValue(date1);
+  if (date2Ms && date1Ms) {
+    let dif = date2Ms - date1Ms; //Faccio differenza
 
-    let difference = Math.round(dif / (1000 * 60 * 60 * 24))//Divido la differenze tra le due date (in ms) per i millisecondi contenuti in 14 ore, poi arrotondo per eccesso con Math.round()
+    let difference = Math.round(dif / (1000 * 60 * 60 * 24)); //Divido la differenze tra le due date (in ms) per i millisecondi contenuti in 14 ore, poi arrotondo per eccesso con Math.round()
     console.log(difference);
-    res.textContent = `La differenza tra le due date è di ${difference === 1 ? difference + ' giorno' : difference + ' giorni'}.`
-})
+    res.textContent = `La differenza tra le due date è di ${
+      difference === 1 ? difference + " giorno" : difference + " giorni"
+    }.`; //Controllo se i giorni sono superiori a 1
+  }else{
+    res.textContent = 'Compila entrambe le date!'
+  }
+});
