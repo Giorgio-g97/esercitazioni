9e5cc0f6654e2476a7b04387bb91e71393fc17c1
Giorgio-g97 <giorgio.g97@gmail.com>
2024-03-23 11:47:45 +0100

    Fatto mescolaCarte()

---
+++ javascript-esercitazioni/esercizi-midali/08-mazzo-carte/script.js
@@ -0,0 +1,36 @@
+class Deck{
+    
+    constructor(){
+        this.cards = [1,2,3,4,5,6,7,8,9,'fante', 'donna', 're'];//Settiamo le carte (in questo caso sono carte francesi)
+        // console.log('Genero nuovo mazzo: ', this.cards);
+        this.mescolaCarte()//Genero funzione mischia carte
+        this.pescaCarta()
+        // this.mettiSotto(cards)
+    }
+
+    mescolaCarte(cards){
+        this.cards.sort((a,b) => {//Vengono paragonati i primi due elementi dell'array del tipo prima "b" e poi "a" (es. 8,5 - 2,8 - 7,2 ecc...)
+//RICORDATI che vengono trasformati in STRINGA
+// console.log(a);
+// console.log(b);
+//Abbiamo 3 casi:
+// 1) res < 0 - a viene messo prima di b
+// 2) res = 0 - non cambia nulla
+// 3) res > 0 - b viene messo prima di a
+        // const res = a - b
+// Seguendo la logica di prima, verrà ordinato in ordine crescente (fai calcolo e verifica)
+//Per generare ordine random utilizziamo questa formula:
+        const res = 0.5 - Math.random();
+            return res;
+        })
+        console.log('Mazzo mischiato: ', this.cards);
+    }
+
+    pescaCarta(){
+        this.cards.shift()
+        console.log('Carte dopo la pescata: ', this.cards);
+    }
+
+}
+
+const cards1 = new Deck()
