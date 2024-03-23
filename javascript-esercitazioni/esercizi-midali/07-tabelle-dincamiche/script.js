92df867bdc054d8553482041d3909007957d0f9b
Giorgio-g97 <giorgio.g97@gmail.com>
2024-03-23 09:12:24 +0100

    Commenti es. 07

--- javascript-esercitazioni/esercizi-midali/07-tabelle-dincamiche/script.js
+++ javascript-esercitazioni/esercizi-midali/07-tabelle-dincamiche/script.js
@@ -1,31 +1,32 @@
 import { users } from "./users.js";
 
 class Tabella {//Creo la classe Tabella in modo da avere un format pre-impostato quando creo le tabelle
+
   //Proprietà
   nomiColonne;
   data;
 
   //Constructor
   constructor(nomiColonne, data) {
-    //this.nomiColonne (inteso nel contesto della classe) lo assegno alla proprietà nomiColonne (che sarà uguale al parametro che imposteremo, cioè "nomiColonne1")
+    //this.nomiColonne (inteso nel contesto della classe) lo assegno alla proprietà nomiColonne (che sarà uguale al parametro che imposteremo dopo, cioè "nomiColonne1")
     this.nomiColonne = nomiColonne;
     //Stessa cosa per le righe di dati
     this.data = data;
-    // console.log(this);
     //Facciamo partire funzione generaTabella() a schermo
     this.generaTabella();
   }
 
+//Funzioni
   generaTabella() {
     const tab = document.createElement("table");
-    tab.appendChild(this.generaColonne());
-    tab.appendChild(this.generaRighe());
+    tab.appendChild(this.generaColonne());// Mi ritorna il thead
+    tab.appendChild(this.generaRighe());// Mi ritorna il tbody
     document.body.appendChild(tab);
   }
 
   generaColonne() {
     const thead = document.createElement("thead"); //Generiamo il thead
-    this.nomiColonne.forEach((head) => {
+    this.nomiColonne.forEach((head) => {//.forEach() perché array
       //Per ogni nomecolonna generato dall'array
       const th = document.createElement("th"); //crea un th
       th.textContent = head; //Con contenuto all'interno dell'array
@@ -36,17 +37,17 @@ class Tabella {//Creo la classe Tabella in modo da avere un format pre-impostato
   }
   generaRighe() {
     const tbody = document.createElement("tbody");
-    this.data.forEach((r) => {
-      //   console.log(r);
+    this.data.forEach((r) => {//Ogni singolo oggetto dell'array è una riga
+      //   console.log(r);// "r" è il singolo oggetto
       const tr = document.createElement("tr");
-      const prova = Object.keys(r)//Mi ritorna un array con le proprietà
+      Object.keys(r)//Mi ritorna un array con le proprietà
         .forEach((key) => {//Ora posso iterare con .forEach()
         // console.log(r[key]);
         const cell = document.createElement("td");
-        cell.textContent = r[key];
-        tr.appendChild(cell);
+        cell.textContent = r[key];//Prendo alla proprietà 'r' (es. prop. 1 è id) il suo valore all'interno della chiave
+        tr.appendChild(cell);// Appendo ogni singola cella alla riga
       });
-      tbody.appendChild(tr);
+      tbody.appendChild(tr);// Poi la riga al body
     });
     return tbody;
   }
