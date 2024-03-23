9f170f4b89897cc74c88094bc8760a7df2a33809
Giorgio-g97 <giorgio.g97@gmail.com>
2024-03-23 16:19:59 +0100

    Esercizio 08 finito

--- javascript-esercitazioni/esercizi-midali/08-mazzo-carte/script.js
+++ javascript-esercitazioni/esercizi-midali/08-mazzo-carte/script.js
@@ -3,12 +3,9 @@ class Deck{
     constructor(){
         this.cards = [1,2,3,4,5,6,7,8,9,'fante', 'donna', 're'];//Settiamo le carte (in questo caso sono carte francesi)
         // console.log('Genero nuovo mazzo: ', this.cards);
-        this.mescolaCarte()//Genero funzione mischia carte
-        this.pescaCarta()
-        // this.mettiSotto(cards)
     }
 
-    mescolaCarte(cards){
+    mescolaCarte(){
         this.cards.sort((a,b) => {//Vengono paragonati i primi due elementi dell'array del tipo prima "b" e poi "a" (es. 8,5 - 2,8 - 7,2 ecc...)
 //RICORDATI che vengono trasformati in STRINGA
 // console.log(a);
@@ -27,10 +24,20 @@ class Deck{
     }
 
     pescaCarta(){
-        this.cards.shift()
-        console.log('Carte dopo la pescata: ', this.cards);
+        const card = this.cards.pop()//Prelevo dall'array di carte l'ultima e la salvo per mostrarla dopo
+        console.log('Carta pescata: ', card)//Stampo carta pescata
+        console.log('Carte dopo la pescata: ', this.cards);//Aggiorno la lista delle carte
+        this.mettiSotto(card)
+    }
+    
+    mettiSotto(card){
+        this.cards.unshift(card);
+        console.log('Carte dopo aver messo sotto: ', this.cards)
     }
 
 }
 
 const cards1 = new Deck()
+
+cards1.mescolaCarte()
+cards1.pescaCarta()
