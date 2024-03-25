4f43e599fa57c4888073a1a1327a231e15139103
Giorgio G <giorgio.g97@gmail.com>
2024-03-25 10:56:17 +0100

    In sospeso

--- javascript-esercitazioni/esercizi-midali/09-ricerca-autocompletamento/script.js
+++ javascript-esercitazioni/esercizi-midali/09-ricerca-autocompletamento/script.js
@@ -1,9 +1,23 @@
 const nomi = ["Giacomo", "Nunzio", "Giorgio", "Mario"];
 
 const searchInput = document.querySelector(".inputSearch");
+const ris = document.querySelector(".res-search");
 
-searchInput.addEventListener("onkeydown", () => {
-  searchValue = searchInput.value;
-
+searchInput.addEventListener("keypress", (e) => {
+  searchValue = e.key;
   console.log(searchValue);
+
+  for (el of nomi) {
+    let elSplitted = el.split("");
+    let wordMatch = '';
+    for (word of elSplitted) {
+      if (e.key == word) {
+        wordMatch += word;
+        console.log(wordMatch);
+        const p = document.createElement("p");
+        ris.appendChild(p);
+        p.textContent = el;
+      }
+    }
+  }
 });
