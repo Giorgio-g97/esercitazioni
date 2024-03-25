6781e7f4f0fef996b3757c9db65ee42449facada
Giorgio G <giorgio.g97@gmail.com>
2024-03-25 18:00:25 +0100

    Da finire a casa

--- javascript-esercitazioni/esercizi-midali/09-ricerca-autocompletamento/script.js
+++ javascript-esercitazioni/esercizi-midali/09-ricerca-autocompletamento/script.js
@@ -9,19 +9,9 @@ searchInput.addEventListener("keypress", (e) => {
   ris.classList.add("border-2", "p-2");
   searchValue = e.key;
   console.log(searchValue);
-
-  nomi.forEach((el) => {//Per ogni elemento all'interno dell'array di nomi
-    let elSplitted = el.split(""); //Splitta ogni lettera
-    let wordMatch = "";
-    for (letter of elSplitted) {
-      //Per ogni lettera della singola parola
-      if (e.key == letter) {
-        wordMatch += letter;
-        console.log(wordMatch);
-        const li = document.createElement("li");
-        ul.appendChild(li);
-        li.textContent = el;
-      }
-    }
+  let sugg = ''
+  nomi.forEach((nome) => {//Per ogni nome all'interno dell'array di nomi
+    sugg = `<li>${nome}</li>`
   });
+  ris.innerHTML = `<ul>${sugg}</ul>`
 });
