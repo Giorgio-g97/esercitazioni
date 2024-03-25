127a622d127ed95d01c4fb0cedf3c5debd8f06dc
Giorgio G <giorgio.g97@gmail.com>
2024-03-25 13:18:21 +0100

    Da completare, minuto 17 lesson09

--- javascript-esercitazioni/esercizi-midali/09-ricerca-autocompletamento/script.js
+++ javascript-esercitazioni/esercizi-midali/09-ricerca-autocompletamento/script.js
@@ -2,22 +2,26 @@ const nomi = ["Giacomo", "Nunzio", "Giorgio", "Mario"];
 
 const searchInput = document.querySelector(".inputSearch");
 const ris = document.querySelector(".res-search");
+const ul = document.createElement("ul");
+ris.appendChild(ul);
 
 searchInput.addEventListener("keypress", (e) => {
+  ris.classList.add("border-2", "p-2");
   searchValue = e.key;
   console.log(searchValue);
 
-  for (el of nomi) {
-    let elSplitted = el.split("");
-    let wordMatch = '';
-    for (word of elSplitted) {
-      if (e.key == word) {
-        wordMatch += word;
+  nomi.forEach((el) => {//Per ogni elemento all'interno dell'array di nomi
+    let elSplitted = el.split(""); //Splitta ogni lettera
+    let wordMatch = "";
+    for (letter of elSplitted) {
+      //Per ogni lettera della singola parola
+      if (e.key == letter) {
+        wordMatch += letter;
         console.log(wordMatch);
-        const p = document.createElement("p");
-        ris.appendChild(p);
-        p.textContent = el;
+        const li = document.createElement("li");
+        ul.appendChild(li);
+        li.textContent = el;
       }
     }
-  }
+  });
 });
